676ff7791d2591ce4089cf90849b2b82620e83c7
wanachengwang <wana.chengwang@gmail.com>
2018-03-02 10:24:01 +0800

    change Monster'modelId

--- DemoServer/scripts/data/d_entities.py
+++ DemoServer/scripts/data/d_entities.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # 10000000 NPC
-# 20000000 Monster
+# 20000000 Monster : here ModelId=20002001 means dragon which has spcial skills and attackDist
 # ext
 
 datas={
@@ -21,15 +21,15 @@ datas={
 	10002026: {'id': 10002026, 'name': '屋门6', 'runSpeed': 0, 'entityType': 'Gate', 'moveSpeed': 0, 'dialogID': 10001001, 'modelID': 10001001, 'etype': 1}, 
 	10002031: {'id': 10002031, 'name': '副本门1', 'runSpeed': 0, 'entityType': 'Gate', 'moveSpeed': 0, 'dialogID': 10001001, 'modelID': 10001001, 'etype': 1}, 
 	10002032: {'id': 10002032, 'name': '副本门2', 'runSpeed': 0, 'entityType': 'Gate', 'moveSpeed': 0, 'dialogID': 10001001, 'modelID': 10001001, 'etype': 1}, 
-	20001001: {'id': 20001001, 'name': '怪物1', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
-	20001002: {'id': 20001002, 'name': '怪物2', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
-	20001003: {'id': 20001003, 'name': '怪物3', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
-	20001004: {'id': 20001004, 'name': '怪物4', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
-	20001005: {'id': 20001005, 'name': '怪物5', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
-	20001006: {'id': 20001006, 'name': '怪物6', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
-	20001007: {'id': 20001007, 'name': '怪物7', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
-	20001008: {'id': 20001008, 'name': '怪物8', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
-	20001009: {'id': 20001009, 'name': '怪物9', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
+	20001001: {'id': 20001001, 'name': '怪物1', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
+	20001002: {'id': 20001002, 'name': '怪物2', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
+	20001003: {'id': 20001003, 'name': '怪物3', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
+	20001004: {'id': 20001004, 'name': '怪物4', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
+	20001005: {'id': 20001005, 'name': '怪物5', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
+	20001006: {'id': 20001006, 'name': '怪物6', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
+	20001007: {'id': 20001007, 'name': '怪物7', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
+	20001008: {'id': 20001008, 'name': '怪物8', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
+	20001009: {'id': 20001009, 'name': '怪物9', 'runSpeed': 65, 'entityType': 'Monster', 'moveSpeed': 50, 'dialogID': 0, 'modelID': 20001001, 'etype': 1}, 
 	20001010: {'id': 20001010, 'name': '怪物Boss1', 'runSpeed': 75, 'entityType': 'Monster', 'moveSpeed': 60, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
 	20001011: {'id': 20001011, 'name': '怪物Boss2', 'runSpeed': 75, 'entityType': 'Monster', 'moveSpeed': 60, 'dialogID': 0, 'modelID': 20002001, 'etype': 1}, 
 }
